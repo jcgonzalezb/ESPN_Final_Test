268f50afc57ecc76cb4dc7f04558f54ccea4b1c2
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-11-12 18:34:23 -0500

    Listener and Reporter files updated

--- src/test/java/org/ESPN/utils/Listener.java
+++ src/test/java/org/ESPN/utils/Listener.java
@@ -1,4 +1,16 @@
 package org.ESPN.utils;
 
-public class Listener {
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.ESPN.reporting.Reporter;
+
+public class Listener implements ITestListener {
+
+    @Override
+    public void onTestSuccess(ITestResult result) {Reporter.info("Test: " + result.getName() + " [PASSED]");}
+
+    @Override
+    public void onTestFailure(ITestResult result){
+        Reporter.error("Test: " + result.getName() + " [FAILED]");
+    }
 }
