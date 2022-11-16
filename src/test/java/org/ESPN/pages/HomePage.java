fe05c1d7736148aeb5564287d6edcf6444578fdd
Juan Camilo Gonzalez <jcgonzalezb@gmail.com>
2022-11-16 16:58:24 -0500

    All Test operational and fixed! Pending requirements for Log In test and Deactivate Account Test.

--- src/test/java/org/ESPN/pages/HomePage.java
+++ src/test/java/org/ESPN/pages/HomePage.java
@@ -1,7 +1,5 @@
 package org.ESPN.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
--- src/test/java/org/ESPN/pages/WatchPage.java
+++ src/test/java/org/ESPN/pages/WatchPage.java
@@ -1,11 +1,10 @@
 package org.ESPN.pages;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WatchPage extends BasePage{
